e43a4a51b6ab1c2975b6c9f507bdddffb71e0241
Paul Zuliani <paulzuliani@gmail.com>
2025-11-05 10:13:26 +0100

    feat: add turnScannerOffAfterDelay snippet - Wrap the async method in a ScannerController class so the method shorthand syntax parses at top level and  resolves.

---
+++ turnScannerOffAfterDelay.js
@@ -0,0 +1,27 @@
+/**
+ * Turn Scanner Off after a delay
+ */
+class ScannerController {
+  timeoutId = null;
+
+  async turnScannerOffAfterDelay(delay_in_ms = 2 * 60 * 1000) {
+    console.log("call turnScannerOffAfterDelay", delay_in_ms);
+    console.log("ClearTimeout id=", this.timeoutId);
+    clearTimeout(this.timeoutId); // clear previously set timeout
+    const delayPromiseToHideScanner = (ms) =>
+      new Promise((res) => {
+        this.timeoutId = setTimeout(res, ms);
+        console.log("SET Timeout id=", this.timeoutId);
+      });
+    const hideScannerAfterDelay = async (delayBy = 60 * 1000) => {
+      await delayPromiseToHideScanner(delayBy);
+      console.log(
+        `turning Scanner Off After Delay ${delayBy} on timeoutId=${this.timeoutId}`,
+      );
+      this.timeoutId = null;
+      return await this.hideScanner();
+    };
+    // return await hideScannerAfterDelay(delay_in_ms)
+    hideScannerAfterDelay(delay_in_ms); // do not return or await, to get asynchronous behavior
+  }
+}
